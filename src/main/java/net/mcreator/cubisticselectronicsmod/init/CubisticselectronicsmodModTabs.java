5e263bd4cc51c0a711e669125bfe34d6ead21036
MrAhmalo <ahmalo456@gmail.com>
2022-02-08 14:41:17 +0100

    Added ElectronicsCreativeTab V.01.

---
+++ src/main/java/net/mcreator/cubisticselectronicsmod/init/CubisticselectronicsmodModTabs.java
@@ -0,0 +1,30 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.cubisticselectronicsmod.init;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.CreativeModeTab;
+
+public class CubisticselectronicsmodModTabs {
+	public static CreativeModeTab TAB_ELECTRONICS_CREATIVE_TAB;
+
+	public static void load() {
+		TAB_ELECTRONICS_CREATIVE_TAB = new CreativeModeTab("tabelectronics_creative_tab") {
+			@Override
+			public ItemStack makeIcon() {
+				return new ItemStack(Items.AMETHYST_SHARD);
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasSearchBar() {
+				return false;
+			}
+		};
+	}
+}
