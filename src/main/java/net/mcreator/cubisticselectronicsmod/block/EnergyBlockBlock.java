b3fd27e735514da53a867236773c9c9d4db095ae
MrAhmalo <ahmalo456@gmail.com>
2022-02-09 18:26:42 +0100

    Updated Energy Block to V.04. - Added Procedure to OpenFarmGUI

--- src/main/java/net/mcreator/cubisticselectronicsmod/block/EnergyBlockBlock.java
+++ src/main/java/net/mcreator/cubisticselectronicsmod/block/EnergyBlockBlock.java
@@ -4,6 +4,7 @@ package net.mcreator.cubisticselectronicsmod.block;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.block.state.BlockState;
@@ -14,9 +15,14 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.client.Minecraft;
+
+import net.mcreator.cubisticselectronicsmod.procedures.OpenEnergyBlockFarmGUIProcedure;
 
 import java.util.Random;
 import java.util.List;
@@ -58,5 +64,20 @@ public class EnergyBlockBlock extends Block {
 			double dz = (random.nextFloat() - 0.5D) * 0.5D;
 			world.addParticle(ParticleTypes.ELECTRIC_SPARK, x0, y0, z0, dx, dy, dz);
 		}
+	}
+
+	@Override
+	public InteractionResult use(BlockState blockstate, Level world, BlockPos pos, Player entity, InteractionHand hand, BlockHitResult hit) {
+		super.use(blockstate, world, pos, entity, hand, hit);
+		int x = pos.getX();
+		int y = pos.getY();
+		int z = pos.getZ();
+		double hitX = hit.getLocation().x;
+		double hitY = hit.getLocation().y;
+		double hitZ = hit.getLocation().z;
+		Direction direction = hit.getDirection();
+
+		OpenEnergyBlockFarmGUIProcedure.execute(world, x, y, z, entity);
+		return InteractionResult.SUCCESS;
 	}
 }
